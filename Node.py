88fab744bae427e21c09579340dbc3a681861534
AbiArya <abhishek.arya3@yahoo.com>
2017-01-23 14:59:26 -0500

    Add files via upload

---
+++ Node.py
@@ -0,0 +1,13 @@
+class Node:
+    def value(self):
+        return self.Val
+
+    def __init__(self):
+        self.color=(255,255,255)
+        self.block=False
+        self.centerLeft=(0,0)
+        self.centerTop=(0,0)
+        self.centerRight=(0,0)
+        self.centerBot=(0,0)
+        self.topLeft=(0,0)
+        self.hasRiver=False
